6ca37c1b83b17f9366dfc8215cc9b1cf473d7d28
vadimvinokurov <vad9fly@gmail.com>
2024-01-29 19:09:55 +0300

    using premake

---
+++ premake5.lua
@@ -0,0 +1,119 @@
+-- premake5.lua
+workspace "vengine"
+	architecture "x64"
+	configurations 
+	{
+		"Debug",
+		"Release",
+		"Profile",
+		"Retail"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "vengine"
+	location "vengine"
+	kind "SharedLib"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/lib3dpart/spdlog/include"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+		defines
+		{
+			"VE_PLATFORM_WINDOWS",
+			"VE_BUILD_DLL",
+			"VE_IS_ENGINE_REGION"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+	filter "configurations:Debug"
+		defines "VE_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "VE_RELEASE"
+		symbols "On"
+		optimize "On"
+		
+	filter "configurations:Profile"
+		defines "VE_PROFILE"
+		optimize "On"
+	
+	filter "configurations:Retail"
+		defines "VE_RETAIL"
+		optimize "On"
+	
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"vengine/lib3dpart/spdlog/include",
+		"vengine/src"
+	}
+	
+	links
+	{
+		"vengine"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+		defines
+		{
+			"VE_PLATFORM_WINDOWS",
+		}
+	
+	filter "configurations:Debug"
+		defines "VE_DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "VE_RELEASE"
+		symbols "On"
+		optimize "On"
+		
+	filter "configurations:Profile"
+		defines "VE_PROFILE"
+		optimize "On"
+	
+	filter "configurations:Retail"
+		defines "VE_RETAIL"
+		optimize "On"
+	
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"
